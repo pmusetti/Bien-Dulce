c1bcef9fc9790abbf5e4edf152c3d16d7496497f
pmusetti <pmusettimillan@gmail.com>
2022-08-08 22:36:30 -0300

    added client options output

--- js/main.js
+++ js/main.js
@@ -147,8 +147,18 @@ function mostrarCarrito() {
         list += item.title + "\n"
     }
     total = Math.trunc(total * 0.8)
-    alert("En tu carrito tenes: \n\n"+ list + "\n Total a pagar $"+total)
+    alert("En tu carrito tenes: \n\n" + list + "\n Total a pagar $" + total)
+
+
 }
+
+
+/********************************
+
+
+*/
+
+
 
 
 
@@ -195,6 +205,16 @@ const precios = {
     }
 }
 
+//Objeto para guardar los datos de la torta a cotizar
+let tortaACotizar = {
+    bizcochuelo: "",
+    relleno: "",
+    cobertura: "",
+    decoracion: "",
+    porciones: ""
+}
+
+//Capturar entradas del cuadro de cotizacion
 const botonCotizar = document.querySelector("#cotizarTorta");
 const campoPrecio = document.querySelector("#precioCotizado");
 const inputBizcochuelo = document.querySelector("#bizcochuelo");
@@ -204,13 +224,38 @@ const inputDecoracion = document.querySelector("#decoracion");
 const inputPorciones = document.querySelector("#porciones");
 
 
+
+
+
 botonCotizar.onclick = () => {
+
+    //Guardar datos de la torta a cotizar
+    tortaACotizar.bizcochuelo = inputBizcochuelo.value;
+    tortaACotizar.relleno = inputRelleno.value;
+    tortaACotizar.cobertura = inputCobertura.value;
+    tortaACotizar.decoracion = inputDecoracion.value;
+    tortaACotizar.porciones = inputPorciones.value;
+
+    //Calcular precio de la torta a cotizar
     let precio = 0;
-    precio += precios.bizcochuelo[`${inputBizcochuelo.value}`.toLowerCase()];
+    precio += precios.bizcochuelo[`${tortaACotizar.bizcochuelo}`.toLowerCase()];
     precio += precios.relleno[`${inputRelleno.value}`.toLowerCase().replaceAll(" ", "")];
     precio += precios.cobertura[`${inputCobertura.value}`.toLowerCase().replaceAll(" ", "")];
     precio += precios.decoracion[`${inputDecoracion.value}`.toLowerCase().replaceAll(" ", "")];
     precio = precio * precios.porciones[inputPorciones.value];
-    campoPrecio.value = "$"+precio;
+    campoPrecio.value = "$" + precio;
+
+
+    mostrarTortaCotizada();
+
+}
+
+//Renderizar un texto con el resultado final de la torta elegida por el cliente y una nota de disclaimer
+const mostrarTortaCotizada = () => {
+    const userOptions = document.querySelector("#userOption");
+    const note = document.querySelector("#nota");
+
+    userOptions.innerText = `La torta que elegiste es para ${tortaACotizar.porciones} personas, con bizcochuelo de ${tortaACotizar.bizcochuelo} rellena de ${tortaACotizar.relleno}, con cobertura de ${tortaACotizar.cobertura} y decorada con ${tortaACotizar.decoracion}.`;
+    note.innerText = "Nota: La siguiente simulacion no debe ser tomada como presupuesto final, sino simplemente como precio orientativo. Por mas detalles contactanos por whatsapp o telefono y te asesoraremos."
 
 }
